1163e92bfbdc19bf52ad9e7f2ef6eb23a35a927f
alsygaev <alsygaev@yandex.ru>
2024-11-29 15:32:19 +0300

    Исправил замечания ревью:  *Пройтись сначала по мапе и удалить из истории таски содержащиеся в мапе, а уже потом делать tasks.clear();

--- src/InMemoryTaskManager.java
+++ src/InMemoryTaskManager.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
+
 
 public class InMemoryTaskManager implements TaskManager, HistoryManager {
 
